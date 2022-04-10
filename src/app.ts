3a9b2b28ecaac6de8a7a41bf2b85d81f04ac7856
Ponder <mail@ponder.codes>
2022-04-09 21:30:50 -0400

    start serving OpenAPI docs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "US Cities API",
+        version: "1.0.0",
+      },
+    },
+    apis: ["./dist/routes/*.js"]
+  };
+const specs = swaggerJsdoc(options);
+const swaggerRoutes = swaggerUi.setup(specs, {explorer: true});
+
 const app = express();
+app.use("/docs", swaggerUi.serve);
+app.get("/docs", swaggerRoutes);
 
-
+const helloRoutes = require('./routes/hello');
+app.use("/hello", helloRoutes);
 
 module.exports = app;
