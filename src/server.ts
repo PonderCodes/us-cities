957e13dcd40efae6261c22768115ef0b961ea907
Ponder <mail@ponder.codes>
2022-04-09 20:48:19 -0400

    changes to get server up and running

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.port || 8080;
-const server = htttp.createServer(app);
+const server = http.createServer(app);
 server.listen(port);
