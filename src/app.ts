027fe8c3247204f8d26f0c888d804541cff9348c
Ponder <mail@ponder.codes>
2022-04-27 11:50:53 -0400

    add CORS allowances

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import { CitiesRouter } from './Cities';
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+  next();
+});
 app.use("/", CitiesRouter);
 
 module.exports = app;
